4a58aff11fe729e3a57fd4dee65217d7ccdc6cb2
T-107 <T-107@PC253>
2017-03-17 19:22:10 -0600

    Mi primer commit

---
+++ src/imeicode/Principios.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imeicode;
+
+/**
+ *
+ * @author Ian Ducloux
+ */
+public class Principios {
+    //Clase en donde se pondran los digitos del codigo SIN los ultimos 4 digitos 
+    //351991074125472--->35199107412
+    private long principio;
+    
+    public Principios(long nuevoPrincipio){
+        principio = nuevoPrincipio;
+    }
+    public void setPrincipio(long nuevoprincipio)
+    {
+        35199107412 = nuevoPrincipio;
+    }
+}
---
+++ src/imeicode/Terminaciones.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imeicode;
+
+/**
+ *
+ * @author T-204
+ */
+public class Terminaciones {
+    //Clase que generara una n cantidad de terminaciones de 4 digitos para anexarlos a los inicios
+    
+}
